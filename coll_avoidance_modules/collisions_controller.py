8e9d71330f1fedb59351e5cc556ce42aa62728bf
LAAS-PC <thibault.noel@laas.fr>
2020-10-01 10:13:07 +0200

    update and place controller in sep. file, first emergency cond+behavior

---
+++ coll_avoidance_modules/collisions_controller.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# Compute a viscoelastic repulsive torque for a list of collisions results (distances + jacobians)
+def computeRepulsiveTorque(q, vq, collDistances, collJacobians, dist_thresh=0.1, kp=0, kv=0):
+    # Initialize repulsive torque
+    tau_avoid = np.zeros(len(q))
+
+    # Loop through the distance to check for threshold violation
+    for i in range(len(collDistances)):
+        J = collJacobians[i]
+        d = collDistances[i]
+
+        tau_rep = np.zeros(len(q))
+        # If violation, compute viscoelastic repulsive torque along the collision jacobian
+        if(d < dist_thresh):
+            tau_rep = -kp*(d - dist_thresh) - kv*J@vq
+        tau_avoid += tau_rep*J.T
+    
+    return tau_avoid
+
+
+def computeEmergencyTorque(vq, kv):
+    return -kv*vq
+
+
+# Compute a condition to switch to the emergency behavior
+# How to deal with v ? 
+def emergencyCondition(collDistances, vq, tau_q, d_thresh, tau_thresh):
+    if(np.min(collDistances) < d_thresh or np.max(np.abs(tau_q)) > tau_thresh):
+        return True
+    return False
