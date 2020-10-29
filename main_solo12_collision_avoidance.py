43aadc4d864afbc3d85316538dbbaa58b3129717
LAAS-PC <thibault.noel@laas.fr>
2020-10-29 17:52:56 +0100

    add functions for traj. replay and init config before ref. traj

--- main_solo12_collision_avoidance.py
+++ main_solo12_collision_avoidance.py
@@ -12,6 +12,43 @@ from time import clock, sleep
 from solo12 import Solo12
 
 
+def compute_pd(q_desired, v_desired, KP, KD, device, active_dof=[]):
+    if(len(active_dof) > 0):
+        arr_KP = np.zeros(12)
+        arr_KV = np.zeros(12)
+        arr_KP[active_dof] = KP
+        arr_KV[active_dof] = KV
+
+	pos_error = q_desired - device.q_mes
+	vel_error = v_desired - device.v_mes
+	tau = KP * pos_error + KD * vel_error #+ KT * tau_desired
+	#tau = np.maximum(np.minimum(tau, tau_max), -tau_max) 
+	return tau
+
+
+def put_on_the_floor(device, q_init):
+	global key_pressed
+	key_pressed = False
+	Kp_pos = 3.
+	Kd_pos = 0.01
+	imax = 3.0
+	pos = np.zeros(device.nb_motors)
+	for motor in range(device.nb_motors):
+		pos[motor] = q_init[device.motorToUrdf[motor]] * device.gearRatioSigned[motor]
+	listener = keyboard.Listener(on_press=on_press)
+	listener.start()
+	print("Put the robot on the floor and press Enter")
+	while not key_pressed:
+		device.UpdateMeasurment()
+		for motor in range(device.nb_motors):
+			ref = Kp_pos*(pos[motor] - device.hardware.GetMotor(motor).GetPosition() - Kd_pos*device.hardware.GetMotor(motor).GetVelocity())
+			ref = min(imax, max(-imax, ref))
+			device.hardware.GetMotor(motor).SetCurrentReference(ref)
+		device.SendCommand(WaitEndOfCycle=True)
+
+	print("Start the motion.")
+
+
 def example_script(name_interface, legs_clib_path, shd_clib_path):
     device = Solo12(name_interface,dt=0.001)
     nb_motors = device.nb_motors
@@ -24,11 +61,15 @@ def example_script(name_interface, legs_clib_path, shd_clib_path):
         qc = QualisysClient(ip="140.93.16.160", body_id=0) # ??
         logger = Logger(device, qualisys=qc, logSize=50000)
     
+    #### Set ref. traj. PD parameters
+    ref_traj_KP = 0
+    ref_traj_KV = 0
+    active_dof = [0,1,2,3,4,5,6,7,8,9,10,11]
+
     #### Set collision avoidance parameters
     legs_threshold = 0.05
     legs_kp = 20.
     legs_kv = 0.0
-
     nb_legs_pairs = 20
 
     #### Shoulder collision parameters
@@ -52,6 +93,8 @@ def example_script(name_interface, legs_clib_path, shd_clib_path):
         viewer_coll = viewerClient(nb_legs_pairs, 3, legs_threshold, shd_threshold, urdf="/home/ada/git/tnoel/solopython/coll_avoidance_modules/urdf/solo12_simplified.urdf", modelPath="/home/ada/git/tnoel/solopython/coll_avoidance_modules/urdf")
 
     device.Init(calibrateEncoders=True)
+
+    put_on_the_floor(device, q_init)
     #CONTROL LOOP ***************************************************
     tau_q = np.zeros(nb_motors)
     while ((not device.hardware.IsTimeout()) and (clock() < 120) and emergencyFlag==0):
